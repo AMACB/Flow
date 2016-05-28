cfd3bd8a0e6c2108827bf4c65a09b1b6a12db760
HankoTanks <hank@modernatomic.com>
2016-05-28 15:11:45 -0700

    Updated compiled.java

--- FlowLanguage/src/flow/compiled/Compiled.java
+++ FlowLanguage/src/flow/compiled/Compiled.java
@@ -1,6 +1,9 @@
 package flow.compiled;
 
-public class Compiled {
-	//Testing testing 123
+public abstract class Compiled {
+	/*
+	 * Psuedo-Compilation for Tokenised code.
+	 * Returns a simpler version of source code.
+	 */
 
 }
