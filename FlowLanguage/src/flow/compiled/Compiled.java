be0d8651bf3d86f2538f1c6187e3906123cd4049
HankoTanks <hank@modernatomic.com>
2016-05-28 15:18:57 -0700

    Foo

--- FlowLanguage/src/flow/compiled/Compiled.java
+++ FlowLanguage/src/flow/compiled/Compiled.java
@@ -1,9 +1,5 @@
 package flow.compiled;
 
 public abstract class Compiled {
-	/*
-	 * Psuedo-Compilation for Tokenised code.
-	 * Purpose: returns code easier for the interpreter to handle.
-	 */
-
+	//foo-bar-baz
 }
