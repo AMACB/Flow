07ba1b36d39696678b29b26e94de6997a1cdf26f
Alexander Burton <amacburton@gmail.com>
2016-05-29 14:17:58 -0500

    Tokenization now works correctly.

---
+++ FlowLanguage/src/flow/token/TokenizeException.java
@@ -0,0 +1,8 @@
+package flow.token;
+
+public class TokenizeException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public TokenizeException(String message) {
+		super(message);
+	}
+}
